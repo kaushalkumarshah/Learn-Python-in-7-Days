ef4c4d36559d6f9ab5b09d5b624d71766d7bd900
kaushalkumarshah <kaushalkumarshah@yahoo.com>
2020-03-01 19:12:43 +0545

    Update excercise2.py

--- Chapter03/excercise2.py
+++ Chapter03/excercise2.py
@@ -1,6 +1,5 @@
-url_tup = ('www', 'thapar', 'edu','index', 'php','about-us','mission')
-url1 = ".".join(url_tup[:3])
-
-url2 = "/".join(url_tup[3:])
-
-print url1+"/"+url2
+tup = ('www', 'thapar', 'edu','index','php','about-us','mission')
+url1= ".".join(tup[0:3])
+url2= ".".join(tup[3:5])
+url3= "/".join(tup[5:])
+print url1+"/"+url2+"/"+url3
